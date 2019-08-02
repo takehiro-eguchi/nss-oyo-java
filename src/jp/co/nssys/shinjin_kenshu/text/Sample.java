0dc3852dfa5d349e69a6f92535cc2f9cfe9694b8
Takehiro Eguchi <t-eguchi@nssys.co.jp>
2019-08-02 09:19:17 +0900

    update

--- src/jp/co/nssys/shinjin_kenshu/text/Sample.java
+++ src/jp/co/nssys/shinjin_kenshu/text/Sample.java
@@ -8,26 +8,12 @@ public class Sample {
 		String[] names = {"#", "Title", "Author", "State  " };
 		Object[][] data = {
 				{1,"Guernica","Picasso", "lending"},
-				{1,"","", ""},
-				{5,"The Tower of Babel","Bruegel", ""},
+				{2,"Primavera","Botticelli", "lending"},
+				{3,"De Nachtwacht","Rembrandt",""},
+				{4,"Meduse","caravaggio",""},
+				{5,"The Tower of Babel","Bruegel", "lending"},
 		};
 		TextTable table = new TextTable(names, data);
 		table.printTable();
-
-//		List<String> nameList = Arrays.asList(names);
-//		List<List<String>> rowsList = Arrays.asList(
-//				Arrays.asList("1","Guernica","Picasso", "貸出中"),
-//				Arrays.asList("5","The Tower of Babel","Bruegel", " ")
-//				);
-//		Board board = new Board(75);
-//		String tableString = board.setInitialBlock(
-//				new Table(board, 75, nameList, rowsList)
-//					.tableToBlocks()).build().getPreview();
-//		System.out.println(tableString);
 	}
-
-//	#,,,
-//	2,Primavera,Botticelli,1
-//	3,De Nachtwacht,Rembrandt,0
-//	4,Meduse,caravaggio,0
 }
