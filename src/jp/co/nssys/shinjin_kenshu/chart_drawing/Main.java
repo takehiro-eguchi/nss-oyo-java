03bd462d105973c18fe0e9ce3e39680a8a785fe1
t-eguchi <t-eguchi@nssys.co.jp>
2019-08-20 09:50:58 +0900

    update

--- src/jp/co/nssys/shinjin_kenshu/chart_drawing/Main.java
+++ src/jp/co/nssys/shinjin_kenshu/chart_drawing/Main.java
@@ -1,6 +1,6 @@
 package jp.co.nssys.shinjin_kenshu.chart_drawing;
 
-import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 import jp.co.nssys.shinjin_kenshu.chart_drawing.draw.ChartDataDrawer;
 import jp.co.nssys.shinjin_kenshu.chart_drawing.draw.IChartDataDrawer;
@@ -16,14 +16,15 @@ public class Main {
 	 * mainメソッド。
 	 *
 	 * @param args 実行時引数（使用しません）。
+	 * @throws FileNotFoundException
 	 */
-	public static void main(String[] args) {
-		System.out.println("海図データファイルの名前を入力してください。");
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		String filePath = scanner.next();
+	public static void main(String[] args) throws FileNotFoundException {
+//		System.out.println("海図データファイルの名前を入力してください。");
+//		@SuppressWarnings("resource")
+//		Scanner scanner = new Scanner(new FileInputStream("resources/chart.csv"));
+//		String filePath = scanner.next();
 
-		IChartDataReader chartDataReader = new ChartDataFileReader(filePath);
+		IChartDataReader chartDataReader = new ChartDataFileReader("resources/chart.csv");
 		IChartDataDrawer chartDataDrawer = new ChartDataDrawer();
 		ChartDataController chartDataController = new ChartDataController(chartDataReader, chartDataDrawer);
 		chartDataController.drawChart();
